a4604ee525beae451fa846b44c1c570c97c72b2e
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-09 23:20:32 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 01/B. Basic Data Types.cpp
@@ -0,0 +1,25 @@
+// B. Basic Data Types
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>         //  setprecision()
+using namespace std;
+int main()
+{
+    int a;
+    long long b ;
+    char c ;
+    float d;
+    double e = 0.0 ;
+
+    cin >> a >> b >> c >> d >> e ;
+    cout << a << endl;
+    cout << b << endl; 
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+
+    return 0 ;
+}
