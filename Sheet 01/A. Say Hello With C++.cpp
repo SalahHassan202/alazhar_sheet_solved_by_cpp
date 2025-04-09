0b82dd118f85c35f0c7bb94caed97a26aa49e9d8
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-09 23:20:11 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 01/A. Say Hello With C++.cpp
@@ -0,0 +1,13 @@
+// A. Say Hello With C++
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    string word ;      cin >> word ;
+    cout <<"Hello, "<< word << endl;
+
+    return 0 ;
+}
