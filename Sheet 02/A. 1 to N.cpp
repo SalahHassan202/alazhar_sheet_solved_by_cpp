a55feb7f2bf84a58896a7e6f6227f0d100fce46a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-12 23:31:32 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 02/A. 1 to N.cpp
@@ -0,0 +1,18 @@
+// A. 1 to N
+#include <iostream>
+#include <string>
+#include <iomanip>         //  setprecision()
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    long long num ;           cin >> num ;
+    for(int i = 1 ; i <= num ; i++)
+    {
+        cout << i << endl;
+    }
+     
+
+    return 0 ;
+}
