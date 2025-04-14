e0d3cb022fb0cf55bac70b8b7fb26c60aac0bc03
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-14 18:44:57 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 02/M. Lucky Numbers.cpp
@@ -0,0 +1,78 @@
+// M. Lucky Numbers
+#include <iostream>
+#include <string>
+#include <iomanip>         //  setprecision()
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+        
+
+    //   Only one line containing two numbers A  and B   (1≤A≤B≤10^5)
+    
+    // 1    .......   10^5   =====>  100000
+
+    //    numbers 4, 7, 47 and 744 are lucky  
+
+    int luckyNumbers [10000]={0};       // every elment on array is equal zero 
+    int flag =0;                       
+
+    luckyNumbers [4]=1;
+    luckyNumbers [7]=1;
+    luckyNumbers [44]=1;
+    luckyNumbers [47]=1;
+    luckyNumbers [74]=1;
+    luckyNumbers [77]=1;
+    luckyNumbers [444]=1;
+    luckyNumbers [447]=1;
+    luckyNumbers [474]=1;
+    luckyNumbers [477]=1;
+    luckyNumbers [774]=1;
+    luckyNumbers [777]=1;
+    luckyNumbers [747]=1;
+    luckyNumbers [744]=1;
+    luckyNumbers [4444]=1;
+    luckyNumbers [4447]=1;
+    luckyNumbers [4474]=1;
+    luckyNumbers [4474]=1;
+    luckyNumbers [4477]=1;
+    luckyNumbers [4777]=1;
+    luckyNumbers [4744]=1;
+    luckyNumbers [4774]=1;
+    luckyNumbers [4774]=1;
+    luckyNumbers [7474]=1;
+    luckyNumbers [7777]=1;
+    luckyNumbers [7774]=1;
+    luckyNumbers [7747]=1;
+    luckyNumbers [7744]=1;
+    luckyNumbers [4477]=1;
+    luckyNumbers [7477]=1;
+    luckyNumbers [4747]=1;
+    luckyNumbers [7444]=1;
+    luckyNumbers [7447]=1;
+
+
+
+    int num1 , num2 ;
+    cin >> num1 >> num2 ;
+
+    for(int i = num1 ; i <= num2 ; i++)
+    {  
+        if(luckyNumbers[i] == 1)
+        {
+            cout<<i<<" ";        // lucky number    
+            flag=1;
+        }
+    
+    }
+
+    if(flag ==0)             
+    {
+        cout<<"-1"<<endl;      // no lucky number 
+    }
+
+
+
+    return 0 ;
+}
