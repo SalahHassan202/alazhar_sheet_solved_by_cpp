01ded784a4b8219fc79b429d1ab176f896f495ac
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-22 05:04:17 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 02/Z. Three Numbers.cpp
@@ -0,0 +1,29 @@
+// Z. Three Numbers
+#include <iostream>
+#include <string>
+#include <iomanip>                               //  setprecision()
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+   
+    int x ,y;     // x ===> start     ;;;;     y===> end
+    cin>>x>>y;
+    int counter =0;
+
+    for(int i=0 ; i<=x ;i++)
+    {
+        for ( int j =0 ; j<=x ; j++)
+        {
+            if (y-i-j>=0   &&  y -i-j<= x)
+            {
+                counter++;
+            }
+        }
+    }
+    cout<<counter<<endl;
+
+
+    return 0 ;
+}
