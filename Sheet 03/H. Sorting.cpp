24bb489d5713602e397dc6a4a06ba0406fe8ebf6
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-23 17:17:27 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 03/H. Sorting.cpp
@@ -0,0 +1,84 @@
+// H. Sorting
+#include <iostream>
+#include <string>
+#include <iomanip>                               //  setprecision()
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    long long size ;                 cin >> size  ;
+    long long arr[size] ;
+
+    for(int i = 0 ; i < size ; i++)
+    {
+        cin >> arr[i] ;
+    }
+
+
+
+    //     ********** the idea of selection sort **********
+    
+    for(int i=0 ; i<size -1; i++)     // size - 1 =====>  because the last element don't compare with any number
+    {
+        int myValue =arr[i];
+        int flag =0 ;
+        int index =0 ;
+        for(int z=i+1 ;z<size ;z++)   // z =i+1 =====> index of element 
+        {
+
+            if(arr[z]< myValue)
+            {
+                myValue =  arr[z] ;
+                index =z ;   // to get the index of element
+                flag =1;   
+            }
+
+        }
+
+        if(flag == 1)    //  we use a flag because we use a swap or not on every cycle by for statement
+        {
+            // swap 
+            int temp = arr[i];
+            arr[i]= myValue ;
+            arr[index]=temp ;
+        }
+
+
+    }
+
+    for(int i =0 ; i<size ;i++)
+    {
+        cout<< arr[i]<<" ";
+    }
+
+
+
+/*
+
+    ///! another solution with built in function  ( sorting function )
+
+
+    long long size ;                 cin >> size  ;
+    long long arr[size] ;
+
+    for(int i = 0 ; i < size ; i++)
+    {
+        cin >> arr[i] ;
+    }
+
+    sort(arr , arr + size ) ; 
+
+
+    for(int i =0 ; i<size ;i++)
+    {
+        cout<< arr[i]<<" ";
+    }
+
+
+
+
+*/
+
+    return 0 ;
+}
