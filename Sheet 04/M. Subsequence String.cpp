6a1b8ec2c5f587424dc321e6eef8a876247c150e
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-03 23:00:13 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 04/M. Subsequence String.cpp
@@ -0,0 +1,41 @@
+// M. Subsequence String
+#include <iostream>
+#include <string>
+#include <iomanip>                                //  setprecision()
+#include <algorithm> 
+#include <cmath>
+using namespace std;
+int main()
+{
+	string s1;                      cin>>s1;
+	string s2= "hello";     // size of hello is 5    ( five )
+	int i = 0 , j = 0 ;     // counter
+	
+	while(true)
+	{
+		if(i == s1.size()  ||  j == s2.size())    // to stop     // end the program 
+		{
+			break;
+		}
+		
+		if(s1[i] == s2[j])
+		{
+			j++;
+		}
+		i++;
+	}
+
+	if(j == 5 )               // hello == 5 
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+
+
+	return 0 ; 
+}
+	
+	
