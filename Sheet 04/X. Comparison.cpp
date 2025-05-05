ccf38ecfa89e1c00b44a848ea5a0df467b7b7e13
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-05 22:59:19 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 04/X. Comparison.cpp
@@ -0,0 +1,37 @@
+// X. Comparison
+#include <iostream>
+#include <string>
+#include <iomanip>                                //  setprecision()
+#include <algorithm> 
+#include <cmath>
+using namespace std;
+int main()
+{
+	
+	string value , sub1 = "",sub2 ;
+	cin>>value ;  
+
+	string myValue = value;  
+	string smallString = value;
+
+	for(int i = 0; i<value.size()-1 ;i++)
+	{
+		sub1 += value[i]; 
+		myValue.erase(0,1);   // split (delete) the first element in string 
+		sub2 = myValue;   
+
+		// sorting all strings 
+		sort(sub1.begin(),sub1.end()); 
+
+		sort(sub2.begin(),sub2.end());
+
+		smallString = min(smallString,sub1+sub2);
+	}
+	cout<<smallString<<endl;
+
+	return 0 ; 
+}
+	
+	
+
+	
