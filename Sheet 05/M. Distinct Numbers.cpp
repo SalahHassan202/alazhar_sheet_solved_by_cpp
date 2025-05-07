3242c663ae0c49ae28b53b5602fd6d43ca6e24ea
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-07 22:50:08 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 05/M. Distinct Numbers.cpp
@@ -0,0 +1,39 @@
+// M. Distinct Numbers
+#include <iostream>
+#include <string>
+#include <iomanip>                                //  setprecision()
+#include <algorithm> 
+#include <cmath>
+using namespace std;
+
+int count(int arr[] , int size)
+{
+	int counter = 0;
+	for(int i =0 ; i<size ; i++)
+	{
+		if(arr[i] != arr[i+1])
+		{
+			counter++;
+			
+		}
+	}
+	return counter;
+
+}
+
+int main()
+{
+    
+	int size ;      cin>>size ;
+	int arr[size];
+	for(int i =0 ; i<size ; i++)
+	{
+		cin>>arr[i];
+	}
+	sort(arr,arr+size );
+
+	
+    cout<<count(arr, size)<<endl;
+
+    return 0 ;
+}
