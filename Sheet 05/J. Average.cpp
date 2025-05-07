71d698895a297bd7fdc69cbe7ec2dd76b4422b17
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-07 22:49:15 +0200

    Al-Azhar Sheet Solution By C++

---
+++ Sheet 05/J. Average.cpp
@@ -0,0 +1,31 @@
+// J. Average
+#include <iostream>
+#include <string>
+#include <iomanip>                                //  setprecision()
+#include <algorithm> 
+#include <cmath>
+using namespace std;
+
+double sum (int size )
+{
+	double arr[size];
+	double sum = 0.0 ;
+	for(int i =0 ; i< size ;i++)
+	{
+		cin>>arr[i];
+		sum += arr[i];
+
+	}
+
+	return sum ;
+}
+
+int main()
+{
+    
+	int size ;                 cin>>size;
+	
+	cout <<setprecision(8)<< sum(size) / size << endl ;
+
+    return 0 ;
+}
